45fd6a307de4fb512ba5928ac19abd3520d39569
Chris Freas <code@packetbusters.net>
2018-10-03 13:17:47 -0400

    add setup.py and move code to package directory so that this code can be developed in a virtual environment.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cannon_bowel',
+    version='1.0',
+    description='Cannon Bowel Restricted Boltzmann Machine',
+    author='Treadco',
+    packages=['cannon_bowel'],
+    install_requires=['numpy', 'Pillow'], #external packages as dependencies
+)
